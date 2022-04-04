4cfc7e84422342c721299e3fe25830b33d1a0174
Cphil31 <cecilephilippe31@gmail.com>
2022-04-04 15:59:33 +0200

    add second api

--- src/component/home/Card.js
+++ src/component/home/Card.js
@@ -1,14 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const Card = ({town}) => { 
+const Card = ({town}) => {    
+
+    const [countryName,setcountryName] = useState([]);
+    const [dataCountry,setDataCountry] = useState([]);
+
+    useEffect(()=>{
+        if(town.name){
+            axios.get(`https://restcountries.com/v3.1/alpha/${town.sys.country}`) 
+            .then( res => setcountryName(res.data[0].translations.fra.common)  )  
+            axios.get(`https://restcountries.com/v3.1/alpha/${town.sys.country}`)
+            .then( dataCountry => setDataCountry(dataCountry.data[0]) )   
+        }
+    },[town])
     
     return (
+        
         <div className='Card'>
             <h3> {town.name} </h3>  
+            <p class ="arrow" style={{'transform': 'rotate('+town.wind.deg+'deg)'}} >  &#8593; </p>
             <p>{town.main.temp} C° </p>
             <img src={town.name ? "http://openweathermap.org/img/wn/"+town.weather[0].icon+"@2x.png": ""} alt={town.name} /> 
-            <p class ="arrow" style={{'transform': 'rotate('+town.wind.deg+'deg)'}} >  &#8593; </p>
             <img class="flag" src={town.name ? "https://countryflagsapi.com/png/"+town.sys.country : "" } alt={town.name} />
-            <p>Pays : {town.sys.country}</p>
+            {town.name ? <p>Pays : {countryName} </p>: " "}
+            {<p>Capitale : {dataCountry.capital}</p>}  
         </div>
     );
 };
--- src/component/home/Form.js
+++ src/component/home/Form.js
@@ -6,21 +6,21 @@ const Form = () => {
     const [town,setTown]= useState([]) ;
     const [search,setSearch]= useState(null);
     
-    
     useEffect(()=>{
         axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&APPID=cd02f311a655c739700a13f10f3651c3&lang=fr`)
-        .then(res =>
-            setTown(res.data)
-             );
-            
+        .then(res=> setTown(res.data) )
+  
     },[search]);
+
+    
+                    
     return (
         <div className='form'>
               <form action="">
                   <input 
                   type="text" 
                   placeholder="Entrez le nom d'une ville"
-                  onChange={(e) => setSearch(e.target.value)}
+                  onChange={(e) => setSearch(e.target.value+" ")}
                   /> 
               </form>
               {town.name ? <Card key={town.id} town={town} /> : " "}
